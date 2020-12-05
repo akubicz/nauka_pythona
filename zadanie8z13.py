1becd53d31e00705a510f2006bcb2d78115f4231
akubicz <akubicz@users.noreply.github.com>
2020-12-05 14:08:25 +0000

    Aktualizacja zadania8z13 - dodanie obnizki 10%

--- zadanie8z13.py
+++ zadanie8z13.py
@@ -1,9 +1,19 @@
 koszyk = [{'nazwa': 'jablka', 'wartosc': 2},
 {'nazwa': 'gruszki', 'wartosc': 3},
-{'nazwa': 'sliwki', 'wartosc': 2.5}]
+{'nazwa': 'sliwki', 'wartosc': 2.5},
+{'nazwa': 'mleko', 'wartosc': 2.5},
+{'nazwa': 'ser', 'wartosc': 2.5}]
 suma=0
+check_ser=0
+check_mleko=0
 
 for i in koszyk:
     suma+=i['wartosc']
+    if i['nazwa']=='ser': check_ser=1
+    elif i['nazwa']=='mleko': check_mleko=1
 
-print(suma)
+if (check_ser == 1 and check_mleko == 1):
+   suma = 0.9*suma
+   print("UWAGA. Obnizka wartosci koszyka o 10%")
+
+print("Wartosc koszyka {:.2f}".format(suma))
