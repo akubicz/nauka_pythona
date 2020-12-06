50df0d71bd87cc9382626bae44818952938b57f7
akubicz <akubicz@users.noreply.github.com>
2020-12-06 09:23:12 +0000

    Dodanie pliku Zadanie12_R4.py

--- zadanie12.py
+++ zadanie12.py
@@ -2,7 +2,7 @@ def znizka(lista, do_zaplaty):
     rabat=0
 
 #Jesli R1 to nie R2
-    if len(lista) > 3:
+    if len(lista) > 3 and do_zaplaty<500:
         rabat=5
         return rabat
 
@@ -15,9 +15,16 @@ def wartosc_koszyka (lista):
 
     for i in lista:
         suma+=i['cena']*i['ilosc']
+
     bonus=znizka(lista, suma)
+
+    min_cena=min(i['cena'] for i in koszyk)
+    print('Wartosc koszyka obnizona o cene najtanszego produktu (-' + str(min_cena) + ")")
+    suma-=min_cena
+
     suma=(1-bonus*0.01)*suma
     print("Znizka wynosi " + str(bonus) + "%")
+
     return suma
 
 koszyk = [{'nazwa': 'ser', 'cena': 2, 'ilosc': 1},
