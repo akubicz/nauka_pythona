e89b21bbd404857033ce0c593cb636b5400967fe
akubicz <akubicz@users.noreply.github.com>
2020-12-06 11:01:20 +0000

    Dodanie zadania ukrycie hasla

---
+++ przypomnienie1.py
@@ -0,0 +1,6 @@
+input_string = list(input())
+
+for i in range(len(input_string)):
+    input_string[i] = '*'
+
+print (''.join(input_string))
--- zadanie12_all.py
+++ zadanie12_all.py
@@ -21,11 +21,29 @@ def znizka_R1_R2(lista, do_zaplaty, wariant):
 
     else: return rabat
 
+
 def znizka_R3(lista, wariant):
     min_cena=0
+
     if wariant ==3:
         min_cena=min(i['cena'] for i in lista)
     return min_cena
+
+
+def znizka_R4(lista,wariant):
+    lista_posortowana = sorted(lista, key=lambda k: k['cena'])
+    ilosc_sztuk=0
+    rabat =0
+    j_rabat
+    for i in lista_posortowana:
+            ilosc_sztuk+=i['ilosc']
+            if ilosc_sztuk > 2:
+                j_rabat = i['ilosc']//3
+                rabat+=i['cena']*j_rabat
+                print("Rabat wynosi " + str(rabat))
+                ilosc_sztuk=i['ilosc']%3
+    return 0
+
 
 def wartosc_koszyka (lista, wariant):
     suma=0
@@ -58,13 +76,14 @@ if __name__ == "__main__":
         {'nazwa': 'salata', 'cena': 5, 'ilosc': 3},
         {'nazwa': 'jablka', 'cena': 2.4, 'ilosc': 2},
         {'nazwa': 'sokowirowka', 'cena': 1500, 'ilosc':2}]
-    opcja=4
+    opcja=3
 
     #Opcja=1 Rabat za > 3 produkty niezaleznie od wartosci koszyka; R1 wazniejsze od R2
     #Opcja=2 Rabat za >3 produkty lub za wartosc koszyka; R2 wazniejsze od R1
     #opcja=3 Rabat na 1 najtanszy produkt
     #opcja=4 Rabat na co 3 produkt
 
+    znizka_R4(koszyk,opcja)
     print("Wyznaczam wartosc koszyka wedug wariantu " + str(opcja))
 
     print("Wartosc do zaplaty: {:.2f}".format(wartosc_koszyka(koszyk,opcja))+"zl")
