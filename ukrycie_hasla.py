1bab486cba3bcc890502e7ee7505d60fa2c8098a
akubicz <akubicz@users.noreply.github.com>
2020-12-06 11:18:16 +0000

    Aktualizacja zadania ukrycie hasla

--- ukrycie_hasla.py
+++ ukrycie_hasla.py
@@ -1,6 +1,8 @@
-input_string = list(input())
-
-for i in range(len(input_string)):
-    input_string[i] = '*'
-
-print (''.join(input_string))
+if __name__ == '__main__':
+    input_string = list(input())
+    if len(input_string) > 2:
+        for i in range(1,len(input_string)-1):
+            input_string[i] = '*'
+        print (''.join(input_string))
+    else:
+        print ("Haslo za krotkie")
